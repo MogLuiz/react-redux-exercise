1e17dd1bfb3de540fd92b4b81b3a70224c6a22af
Luiz Henrique <luizhjramos@outlook.com>
2021-09-13 20:59:44 -0300

    feat: constantes para os tipos de cations

--- src/store/reducers/numeros.js
+++ src/store/reducers/numeros.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
+import { NUM_MIN_ALTERADO, NUM_MAX_ALTERADO } from '../actions/actionsTypes'
 
 const initialState = {
     min: 1,
@@ -9,12 +10,12 @@ const initialState = {
 export default function(state = initialState, action) {
 
     switch(action.type) {
-        case 'NUM_MIN_ALTERADO':
+        case NUM_MIN_ALTERADO:
             return {
                 ...state,
                 min: action.payload
             }
-        case 'NUM_MAX_ALTERADO':
+        case NUM_MAX_ALTERADO:
             return {
                 ...state,
                 max: action.payload
